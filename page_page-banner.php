ae02e9fc09d1b3f07a094e66ab4d8e69d7d33c5e
purpl3m3r1ngu3 <briancruz_trick@hotmail.com>
2018-03-08 17:20:30 -0700

    Minor Changes
    Changed name of the theme, Got rid of title from a page template, fixed
    css with said page

--- page_page-banner.php
+++ page_page-banner.php
@@ -22,7 +22,6 @@
       <?php } ?>
     </div>
   </div>
-  <h1 class="page-title"><?php the_title(); ?></h1>
   <div class="page-body">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
       the_content();
